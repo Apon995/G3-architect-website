611ce5d5c8dc61f0a40894644c51c3d263695fcf
apon995 <shofikulsamz995@gmail.com>
2023-07-18 14:33:40 +0600

    simple-done

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const sidebarShow=()=>{
+    document.querySelector(".side-bar").style.right = "0px";
+}
+const sidebarHidden=()=>{
+    document.querySelector(".side-bar").style.right = "360px";
+}
